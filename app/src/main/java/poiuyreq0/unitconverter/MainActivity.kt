b7d1ffa137333c12d177bab6f3085cab499f2654
poiuyreq0 <poiuyreq0@gmail.com>
2023-01-24 17:23:26 +0900

    project init

--- app/src/main/java/poiuyreq0/unitconverter/MainActivity.kt
+++ app/src/main/java/poiuyreq0/unitconverter/MainActivity.kt
@@ -2,10 +2,68 @@ package poiuyreq0.unitconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import poiuyreq0.unitconverter.databinding.ActivityMainBinding
+import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var inputNumber: BigDecimal = BigDecimal("0")
+    private var cmToM = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val inputEditText = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val swapImageButton = binding.swapImageButton
+
+        inputEditText.addTextChangedListener { text ->
+            inputNumber = if (text.isNullOrEmpty()) {
+                BigDecimal("0")
+            } else {
+                BigDecimal(text.toString())
+            }
+            if (cmToM) {
+                outputTextView.text = inputNumber.times(BigDecimal("0.01")).toString()
+            } else {
+                outputTextView.text = inputNumber.times(BigDecimal("100")).toString()
+            }
+        }
+
+        swapImageButton.setOnClickListener {
+            cmToM = cmToM.not()
+            if (cmToM) {
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(BigDecimal("0.01")).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(BigDecimal("100")).toString()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean("cmToM", cmToM)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        cmToM = savedInstanceState.getBoolean("cmToM")
+        Log.d("cmToM", cmToM.toString())
+        binding.inputUnitTextView.text = if (cmToM) "cm" else "m"
+        binding.outputUnitTextView.text = if (cmToM) "m" else "cm"
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
